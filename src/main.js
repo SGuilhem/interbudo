a8aaa5fcdde7995d2abeaa559b7078b98e769997
SGuilhem <guilhemsaclier@gmail.com>
2026-06-30 14:50:59 +0200

    fix vueuse in main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import router from './router'
 import './assets/main.css'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import { createHead } from '@vueuse/head'
-
-const head = createHead()
-app.use(head)
 
 const app = createApp(App)
 app.use(router)
